acb12b66f64d1db31b37512d18c19ee800affe5e
libinsocxo <libin@socxo.com>
2024-03-30 22:16:56 +0530

    the card design completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { JoinclassComponent } from './components/joinclass/joinclass.component';
 import { CreateclassComponent } from './components/createclass/createclass.component';
+import { CardComponent } from './card/card.component';
 
 export const routes: Routes = [
     {
         path:'',
-        component:HomeComponent
+        component:CardComponent
     },
     {
         path:'JoinClass',
